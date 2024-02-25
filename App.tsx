96a4b29a18fbc3956376a04e89bbfb3829c16efe
Jalen Wu <jalenwu@Jalens-Air.local.tld>
2024-02-25 00:58:52 -0500

    initialized firebase and added write to firestore db function

--- App.tsx
+++ App.tsx
@@ -1,8 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import Background from './components/background'
+import {write_to_db} from './write_to_db.js';
 
 export default function App() {
+
+  console.log("Writing to database");
+  write_to_db();
   return (
     <View style={styles.container}>
       <Background/>
